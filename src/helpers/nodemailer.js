cd4e16765e44de40369e65eca180323ea5c3d540
Matías Lopez <mts2311@hotmail.com>
2023-06-01 18:24:17 -0300

    add link generate pass gmail

--- src/helpers/nodemailer.js
+++ src/helpers/nodemailer.js
@@ -1,12 +1,12 @@
 const nodemailer = require("nodemailer");
-
+require('dotenv').config()
 module.exports = (usuario) => {
     const transporter = nodemailer.createTransport({
         service: "gmail",
         port: 587,
         auth: {
-            user: "sebastos23@gmail.com",
-            pass: "omlzvmniogukateq",
+            user: process.env.EMAIL_NODEMAILER, //email de gmail del cual se envia el mail
+            pass: process.env.PASS_NODEMAILER, //clave generada desde el link de gmail
         },
     });
     
@@ -15,9 +15,9 @@ module.exports = (usuario) => {
         to: usuario.email,
         subject: "Bienvenido",
         html: `<h1 style="color: blue;">BIENVENIDO ${usuario.nombre}!!!</br><span style="color: green;">Nos agrada contar con vos en nuestro sitio!!!</span></h1>`,
-        attachments : [
+        /* attachments : [
             {path : "../../clasesApoyo/clase11_25-01_concesionarias/public/images/1684370621776_img_.jpg"}
-        ]
+        ] */
     };
     
     try {
@@ -26,3 +26,5 @@ module.exports = (usuario) => {
         console.log(err);
     }
 }
+// link para generar lass contraseñas 
+// https://security.google.com/settings/security/apppasswords
